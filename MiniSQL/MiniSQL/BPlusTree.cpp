afd25824b97e86d071aa23f05a90750d49e2cc44
林 袁 <1075016158@qq.com>
2018-06-11 21:13:24 +0800

    B+Tree Interface

---
+++ MiniSQL/MiniSQL/BPlusTree.cpp
@@ -0,0 +1,33 @@
+#include "BPlusTree.h"
+using namespace std;
+
+template<typename T>
+inline TreeNode<T>::TreeNode(int d, bool leaf)
+	:degree(d),isLeaf(leaf)
+{
+
+}
+
+template<typename T>
+int TreeNode<T>::insert(T & key, int offset)
+{
+	return 0;
+}
+
+template<typename T>
+TreeNode * TreeNode<T>::split(T & key)
+{
+	return nullptr;
+}
+
+template<typename T>
+void TreeNode<T>::remove(int index)
+{
+}
+
+template<typename T>
+bool TreeNode<T>::search(const T & key, int index)
+{
+	return false;
+}
+
--- MiniSQL/MiniSQL/BPlusTree.h
+++ MiniSQL/MiniSQL/BPlusTree.h
@@ -1,19 +1,40 @@
 #pragma once
-#include "MiniSQL.h"
+/*
+BPTree用作IndexManager的索引数据结构，存在内存中，
+关闭程序后再次启动根据index的information重建BPTree
+*/
+#include <iostream>
 #include <vector>
+#include <string>
+#include "MiniSQL.h"
+
 using namespace std;
 
+//Internal和Leaf放在一个类里，用isLeaf区分
 /* TreeNode:base class of nodes of BPlusTree */
 template<typename T>
 class TreeNode {
 public:
-	int size;
+	int size;//size <= 4K
 	int degree;
-	int type_bytes;//当前数据类型的大小
+	//int type_bytes;//当前数据类型的大小
 	bool isLeaf; //type of a Node(internal or leaf)
-	vector<T> keys;
-	vector<TreeNode*> ptrs;
-	
-	TreeNode(); // calculate degree
-	~TreeNode();
+	vector<T> keys;//索引值
+	vector<int> offsets;//offset of the record in its file
+	TreeNode* parent;
+	TreeNode* sibling;
+	vector<TreeNode<T> *> children;//指向孩子结点的数目为keys+1
+	bool isRoot;
+public:
+	TreeNode() = default;
+	TreeNode(int d, bool leaf);
+	int insert(T& key, int offset);
+	TreeNode* split(T& key);//insert后过多而split一个Node
+	void remove(int index);//remove node[index]
+	bool search(const T& key, int index);
+	~TreeNode() {};
 };
+
+
+
+
