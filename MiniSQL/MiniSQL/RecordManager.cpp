446ed345feb8b7e7c568a0eac1de7443dbf7a9c3
林 袁 <1075016158@qq.com>
2018-06-23 13:59:55 +0800

    待测试的（加上IndexManager）的RecordManager

--- MiniSQL/MiniSQL/RecordManager.cpp
+++ MiniSQL/MiniSQL/RecordManager.cpp
@@ -1,4 +1,5 @@
 #include "RecordManager.h"
+#include "MiniSQL.h"
 
 void RecordManager::createTable(string tableName)
 {
@@ -15,7 +16,266 @@ void RecordManager::dropTable(string tableName)
 	return ;
 }
 
-void RecordManager::insertRecord(const Table & table, Row & row)
+//将row（即一条record）插入table对应的最后一个block（在buffer中而非直接在文件中）
+void RecordManager::insertRecord(Table & table, Row & row)
 {
-
+	Block* blockptr = &bufferm->findBlk(table, table.fileTail);
+	char temp[BLOCK_SIZE];
+	int offset = 0;
+	//把row中的内容按照attribute的类型和顺序译为char数组
+	for (int i = 0; i < table.attriNum; i++) {
+		if (table.attributes[i].type == 0) { //int
+			memcpy(temp + offset, string2int(row.columns[i]), 4);
+			offset += 4;
+		}
+		else if (table.attributes[i].type == -1) {// float
+			memcpy(temp + offset, string2int(row.columns[i]), 4);
+			offset += 4;
+		}
+		else {
+			memcpy(temp + offset, string2int(row.columns[i]), table.attributes[i].type);
+			offset += table.attributes[i].type; //char(n)
+		}
+	}
+	if (blockptr->size + table.recLength + 1 > BLOCK_SIZE) {
+		Block newBlock(table.name, table.blockNum);
+		table.blockNum += 1;
+		table.fileTail += BLOCK_SIZE * (table.blockNum - 1) + table.recLength + 1;
+		newBlock.insertRecord(temp,table.recLength);
+		bufferm->block2buf(*blockptr);
+		return;
+	}
+	//在现有的块中insert
+	blockptr->insertRecord(temp, table.recLength);
+	return;
 }
+
+//先根据cond找出块对应的offset（调用Index）再将record的valid byte置0(在buffer中)
+//同时在indexmanager将其有index的remove掉
+void RecordManager::deleterecord(const Table & table, const vector<Condition> cond)
+{
+	/* 这里condition都是and连接，找到满足条件的一系列offset */
+	set<int > temp;	//找出目标的offsets存于set<int > temp
+	Row row;
+	Block blockptr;
+	SearchResult.clear();
+
+	for (int i = 0; i < cond.size(); i++) {
+		//在这一条件的属性为PK或unique时可以通过IndexManager索引查找
+		if (table.attributes[cond[i].columnNum].PK == true || table.attributes[cond[i].columnNum].unique == true) {
+			if (cond[i].opcode == 4) { //==
+				indexm->selectEqual(table.name, cond[i].attriName, cond[i].cond, table.attributes[cond[i].columnNum].type);
+			}
+			else if (cond[i].opcode == 5) { //!=
+				indexm->selectInequal(table.name, cond[i].attriName, cond[i].cond, table.attributes[cond[i].columnNum].type);
+			}
+			else {
+				indexm->selectRange(table.name, cond[i].attriName, cond[i].cond, cond[i].opcode, table.attributes[cond[i].columnNum].type);
+			}
+			//如果是第一个条件取并集，否则取交集
+			if (i == 0) { 
+				set_union(temp.begin(), temp.end(), SearchResult.begin(), SearchResult.end(), temp.begin());//注意第五个参数的形式
+			}
+			else {
+				set_intersection(temp.begin(), temp.end(), SearchResult.begin(), SearchResult.end(), temp.begin());
+			}
+		}
+		else { //条件的属性为一般属性，遍历读取.dat
+			for (int j = 0; j < table.blockNum; j++) {
+				blockptr = bufferm->readBlock(table, j);
+				for (int byte = 0; byte < BLOCK_SIZE; byte += table.recLength) {
+					if (j*BLOCK_SIZE + byte >= table.fileTail)
+						break;
+					row = byte2row(table, blockptr.content + byte);
+					if (Comparator(table, row, cond) == true) { //符合条件，应该被删除
+						memset(blockptr.content + byte, 0, 1); //将valid位置0
+					}
+				}
+			}
+			return ; //在循环的第一次就return了，因为遍历做的是多条件的判断
+		}
+	}
+
+	//读取Blocks并在buffermanager中把这些offset的valid byte都置0
+	//由IndexManager找到的offset直接就是valid byte所在的位置
+	for (auto elem : temp) {
+		int offrecord  = 0;
+		blockptr = bufferm->findBlk(table, elem);
+		offrecord = elem % BLOCK_SIZE;
+		memset(blockptr.content + offrecord, 0, 1);
+	}
+	return ;
+}
+
+//select将buffer中满足条件的record选出并返回已经转为string的records
+Records& RecordManager::selectRecord(const Table & table, const vector<Condition> cond)
+{
+	Records result;
+	Row row;
+
+	set<int > temp;	//找出目标的offsets存于set<int > temp
+	Block blockptr;
+	SearchResult.clear();
+
+	for (int i = 0; i < cond.size(); i++) {
+		//在这一条件的属性为PK或unique时可以通过IndexManager索引查找
+		if (table.attributes[cond[i].columnNum].PK == true || table.attributes[cond[i].columnNum].unique == true) {
+			if (cond[i].opcode == 4) { //==
+				indexm->selectEqual(table.name, cond[i].attriName, cond[i].cond, table.attributes[cond[i].columnNum].type);
+			}
+			else if (cond[i].opcode == 5) { //!=
+				indexm->selectInequal(table.name, cond[i].attriName, cond[i].cond, table.attributes[cond[i].columnNum].type);
+			}
+			else {
+				indexm->selectRange(table.name, cond[i].attriName, cond[i].cond, cond[i].opcode, table.attributes[cond[i].columnNum].type);
+			}
+			//如果是第一个条件取并集，否则取交集
+			if (i == 0) {
+				set_union(temp.begin(), temp.end(), SearchResult.begin(), SearchResult.end(), temp.begin());//注意第五个参数的形式
+			}
+			else {
+				set_intersection(temp.begin(), temp.end(), SearchResult.begin(), SearchResult.end(), temp.begin());
+			}
+		}
+		else { //条件的属性为一般属性，遍历读取.dat
+			for (int j = 0; j < table.blockNum; j++) {
+				blockptr = bufferm->readBlock(table, j);
+				for (int byte = 0; byte < BLOCK_SIZE; byte += table.recLength) {
+					if (j*BLOCK_SIZE + byte >= table.fileTail)
+						break;
+					row = byte2row(table, blockptr.content + byte);
+					if (Comparator(table, row, cond) == true) { //符合条件，应该被删除
+						result.rows.push_back(row);
+					}
+				}
+			}
+			return result; //在循环的第一次就return了，因为遍历做的是多条件的判断
+		}
+	}
+
+	//读取Blocks并在buffermanager中把这些offset的valid byte都置0
+	//由IndexManager找到的offset直接就是valid byte所在的位置
+	for (auto elem : temp) {
+		int offrecord = 0;
+		blockptr = bufferm->findBlk(table, elem);
+		offrecord = elem % BLOCK_SIZE;
+		row = byte2row(table, blockptr.content + offrecord);
+	}
+	return result;
+}
+
+/*---------------------------辅助函数-------------------------------*/
+//将以字节表示的数据转换为row（string）的形式
+Row RecordManager::byte2row(Table table, char * src)
+{
+	Row row;
+	string temp;
+	src += 1; //overread the valid byte
+	for (int i = 0; i < table.attriNum; i++) {
+		if (table.attributes[i].type == 0) { //INT
+			temp = int2string(src);
+			src += 4;
+		}
+		else if(table.attributes[i].type == -1){ //FLOAT
+			temp = float2string(src);
+			src += 4;
+		}
+		else { //CHAR(n)
+			temp = string(src, table.attributes[i].type);
+			src += table.attributes[i].type;
+		}
+		row.columns.push_back(temp);
+	}
+	return row;
+}
+
+//用于一行record（string）和条件的判断
+bool RecordManager::Comparator(Table table, Row row, vector<Condition> conditions)
+{
+	for (int i = 0; i<conditions.size(); i++) {
+		int colNum = conditions[i].columnNum;
+		string value1 = row.columns[colNum];
+		string value2 = conditions[i].cond;
+		int int1 = atoi(value1.c_str());
+		int int2 = atoi(value2.c_str());
+		float fvalue1 = atof(value1.c_str());
+		float fvalue2 = atof(value2.c_str());
+		switch (table.attributes[colNum].type)
+		{
+		case 0: //INT
+			switch (conditions[i].opcode)
+			{
+			case Lt:
+				if (int1 >= int2) return 0;
+				break;
+			case Le:
+				if (int1 > int2) return 0;
+				break;
+			case Gt:
+				if (int1 <= int2) return 0;
+				break;
+			case Ge:
+				if (int1 < int2) return 0;
+				break;
+			case Eq:
+				if (int1 != int2) return 0;
+				break;
+			case Ne:
+				if (int1 == int2) break;
+				break;
+			}
+			break;
+		case -1: // FLOAT
+			switch (conditions[i].opcode)
+			{
+			case Lt:
+				if (fvalue1 >= fvalue2) return 0;
+				break;
+			case Le:
+				if (fvalue1 > fvalue2) return 0;
+				break;
+			case Gt:
+				if (fvalue1 <= fvalue2) return 0;
+				break;
+			case Ge:
+				if (fvalue1 < fvalue2) return 0;
+				break;
+			case Eq:
+				if (fvalue1 != fvalue2) return 0;
+				break;
+			case Ne:
+				if (fvalue1 == fvalue2) return 0;
+				break;
+			}
+		default: //CHAR(n)
+			switch (conditions[i].opcode)
+			{
+			case Lt:
+				if (value1 >= value2) return 0;
+				break;
+			case Le:
+				if (value1 > value2) return 0;
+				break;
+			case Gt:
+				if (value1 <= value2) return 0;
+				break;
+			case Ge:
+				if (value1 < value2) return 0;
+				break;
+			case Eq:
+				if (value1 != value2) return 0;
+				break;
+			case Ne:
+				if (value1 == value2) return 0;
+				break;
+			}
+			break;
+		}
+	}
+	return 1;
+}
+
+
+
+
+
--- MiniSQL/MiniSQL/RecordManager.h
+++ MiniSQL/MiniSQL/RecordManager.h
@@ -1,38 +1,48 @@
 #pragma once
 /*
-	RecordManager负责管理记录表中的数据文件，主要功能为实现数据文件的创建与删除、
+	recordmanager负责管理记录表中的数据文件，主要功能为实现数据文件的创建与删除、
 	记录的插入、删除和查找操作。其中记录的查找操作要求能支持不带条件的查找和带一
 	个条件的查找（包括等值查找、不等值查找和区间查找）
-	TODO: 在minisql.h中定义condition类
+
+	add:可以将buffermanager和indexmanager设置为全局，在recordmanager中用extern调用
+	查找后indexmanager改写全局的set容器中的返回结果
+
 */
 #include <iostream>
 #include <string>
+#include <set>
+#include <algorithm>
 #include <vector>
 #include <fstream>
-#include "MiniSQL.h"
-#include "BufferManager.h"
-#include "IndexManager.h"s
-using namespace std;
+#include "minisql.h"
+#include "indexmanager.h"
+#include "buffermanager.h"
 
+set<int> SearchResult; //实际定义在Minisql.h中，为IndexManager查询返回的全局容器变量
+using namespace std;
+enum {Gt,Lt,Ge,Le,Eq,Ne};
 class RecordManager
 {
 public:
-	BufferManager *bufferM;
-	IndexManager *indexM;
+	BufferManager *bufferm;
+	IndexManager *indexm;
+public:
+	Row byte2row(Table table, char* src); //将以字节表示的数据转换为row（string）的形式
+	bool Comparator(Table table, Row row, vector<Condition> conditions); //在遍历文件的数据时判断是否满足conditions
 public:
 	RecordManager() = default;
 	RecordManager(BufferManager *b, IndexManager* i)
-		:bufferM(b), indexM(i)
+		:bufferm(b), indexm(i)
 	{};
 	//create和drop仅管理.dat文件的建立与删除,非常simple
-	void createTable(string tableName);
-	void dropTable(string tableName);
+	void createTable(string tablename);
+	void dropTable(string tablename);
 
-	//insert/delete:API调用的接口，插入/删除数据到buffer中
-	//delete和select是RecordManager中的难点
-	void insertRecord(const Table& table, Row& row);
-	//void deleteRecord(const Table& table, const vector<Condition> cond);
-	//void selectRecord(const Table& table, );
+	//insert/delete:api调用的接口，插入/删除数据到buffer中
+	//（务必注意是写到buffer中的块！）
+	void insertRecord(Table& table, Row& row);
+	void deleterecord(const Table& table, const vector<Condition> cond);
+	Records& selectRecord(const Table& table, const vector<Condition> cond);
 	~RecordManager() = default;
 };
 
