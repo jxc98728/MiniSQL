76d24cf2b1f8f81df362bcd3dc020ae1ca04fa39
Chris John <459830713@qq.com>
2018-06-25 16:42:06 +0800

    Add files via upload
    Interpreter（仍需加入更多语法检测）

---
+++ MiniSQL/MiniSQL/Interpreter.cpp
@@ -0,0 +1,634 @@
+#include "Interpreter.h"
+
+using namespace std;
+
+Interpreter::Interpreter()
+{
+	sqlcmd.tableInfo.recLength = 0;
+	sqlcmd.tableInfo.attriNum = 0;
+}
+
+Interpreter::~Interpreter()
+{  
+
+}
+
+void Interpreter::readquery(void)
+{	
+	string temp, s = "";
+	tokenizer.clear();
+
+	while (s == "" || (s.length() > 0 && s[s.length() - 1] != ';')) 
+	{
+		if (s == "")
+			cout << "MiniSQL" << "> ";
+		else
+			cout << "       " << "> ";
+		getline(cin, temp);
+		if(s == "")
+			s = s + trim(temp);
+		else
+			s = s + " " + trim(temp);
+	}
+	query = s;
+}
+
+int Interpreter::parser(void)
+{	
+	sqlcmd.clear();
+
+	vector<string>::iterator it = tokenizer.begin();
+
+	int left = 0;
+	int right = 0;
+
+	if (*it == "create")
+	{
+		it++;
+
+		if (*it == "index") 
+		{
+			sqlcmd.opName = 2;
+			it++;
+			sqlcmd.indexInfo.indexName = *it;
+			it++;
+			if (*it != "on")
+			{
+				cerr << "SQL Syntax Error! " << endl;
+				return 1;
+			}
+			it++;
+			sqlcmd.indexInfo.tableName = *it;
+			it++;
+			if (*it != "(")
+			{
+				cerr << "SQL Syntax Error! " << endl;
+				return 1;
+			}
+			it++;
+			sqlcmd.indexInfo.attributeName = *it;
+			it++;
+			if (*it != ")")
+			{
+				cerr << "SQL Syntax Error! " << endl;
+				return 1;
+			}
+		}
+
+		else if (*it == "table") 
+		{
+			it++;
+			sqlcmd.opName = 0;
+			sqlcmd.tableInfo.name = *it;
+			
+			it++;
+			vector<string> temp;
+
+			while (*it != ";")
+			{
+				
+				if (*it == "(" || *it == ")")
+				{
+					it++;
+					continue;
+				}
+				if (*it == "primary")
+				{
+					it += 3;
+					for (vector<Attribute>::iterator t = sqlcmd.tableInfo.attributes.begin(); t != sqlcmd.tableInfo.attributes.end(); t++)
+					{
+						if (t->name == *it) 
+						{
+							t->PK = true;
+							break;
+						}
+
+					}
+					break;
+				}
+				if (*it == ",") 
+				{
+					if (temp.size() == 2 && temp[1] == "int")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], 0, false, false));
+						sqlcmd.tableInfo.recLength += 4;
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+
+					else if (temp.size() == 2 && temp[1] == "float")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], -1, false, false));
+						sqlcmd.tableInfo.recLength += 4;
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+
+					else if (temp.size() == 3 && temp[1] == "int")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], 0, false, true));
+						sqlcmd.tableInfo.recLength += 4;
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+					else if (temp.size() == 3 && temp[1] == "float")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], -1, false, true));
+						sqlcmd.tableInfo.recLength += 4;
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+					else if (temp.size() == 3 && temp[1] == "char")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], atoi(temp[2].c_str()), false, false));
+						sqlcmd.tableInfo.recLength += atoi(temp[2].c_str());
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+					else if (temp.size() == 4 && temp[1] == "char")
+					{
+						sqlcmd.tableInfo.attributes.push_back(Attribute(temp[0], atoi(temp[2].c_str()), false, true));
+						sqlcmd.tableInfo.recLength += atoi(temp[2].c_str());
+						sqlcmd.tableInfo.attriNum++;
+						temp.clear();
+					}
+					else {
+						cerr << "SQL Syntax Error! " << endl;
+						return 1;
+					}
+				}
+				else 
+				{
+					temp.push_back(*it);
+				}
+				it++;
+			}
+
+		}
+
+		else 
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+	}
+
+	else if (*it == "select")
+	{
+		if (tokenizer.size() < 5)
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+
+		if (tokenizer[1] != "*" || tokenizer[2] != "from")
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+
+		sqlcmd.tableInfo.name = tokenizer[3];
+		sqlcmd.opName = 4;
+
+		if (tokenizer[4] == "where")
+		{
+			it += 5;
+			
+			while (*(it + 3) != ";")
+			{
+				Condition * temp = new Condition;
+				(*temp).attriName = *it;
+				(*temp).cond = *(it + 2);
+				if (*(it + 1) == ">") {
+					(*temp).opcode = 0;
+				}
+				if (*(it + 1) == "<") {
+					(*temp).opcode = 1;
+				}
+				if (*(it + 1) == ">=") {
+					(*temp).opcode = 2;
+				}
+				if (*(it + 1) == "<=") {
+					(*temp).opcode = 3;
+				}
+				if (*(it + 1) == "=") {
+					(*temp).opcode = 4;
+				}
+				if (*(it + 1) == "<>") {
+					(*temp).opcode = 5;
+				}
+				sqlcmd.condList.push_back(*temp);
+				it += 4;
+			}
+			Condition * temp = new Condition;
+			(*temp).attriName = *it;
+			(*temp).cond = *(it + 2);
+			if (*(it + 1) == ">") {
+				(*temp).opcode = 0;
+			}
+			if (*(it + 1) == "<") {
+				(*temp).opcode = 1;
+			}
+			if (*(it + 1) == ">=") {
+				(*temp).opcode = 2;
+			}
+			if (*(it + 1) == "<=") {
+				(*temp).opcode = 3;
+			}
+			if (*(it + 1) == "=") {
+				(*temp).opcode = 4;
+			}
+			if (*(it + 1) == "<>") {
+				(*temp).opcode = 5;
+			}
+			sqlcmd.condList.push_back(*temp);
+		}
+	}
+
+	else if (*it == "drop")
+	{
+		it++;
+
+		if (*it == "table")
+		{
+			sqlcmd.opName = 1;
+			it++;
+			if (*it == ";")
+			{
+				cerr << "SQL Syntax Error! " << endl;
+				return 1;
+			}
+			sqlcmd.tableInfo.name = *it;
+		}
+
+		else if (*it == "index")
+		{
+			sqlcmd.opName = 3;
+			it++;
+			if (*it == ";") {
+				cerr << "SQL Syntax Error! " << endl;
+				return 1;
+			}
+			sqlcmd.indexInfo.indexName = *it;
+		}
+
+		else
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+	}
+
+	else if (*it == "insert")
+	{
+		it++;
+		sqlcmd.opName = 5;
+		if (*it != "into")
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+
+		it++;
+
+		sqlcmd.tableInfo.name = *it;
+
+		it++;
+
+		if (*it != "values")
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+		it++;
+
+		while (*it != ";")
+		{
+			if (*it != "(" || *it != "," || *it != ")")
+			{
+				sqlcmd.valuesList.push_back(*it);
+			}
+			it++;
+		}
+
+	}
+
+	else if (*it == "delete")
+	{
+		it++;
+		if (*it != "from")
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+		it++;
+
+		sqlcmd.tableInfo.name = *it;
+
+		it++;
+
+		if (*it == ";")
+		{
+
+		}
+		else if (*it == "where")
+		{
+			it ++;
+
+			while (*(it + 3) != ";")
+			{
+				Condition * temp = new Condition;
+				(*temp).attriName = *it;
+				(*temp).cond = *(it + 2);
+				if (*(it + 1) == ">") {
+					(*temp).opcode = 0;
+				}
+				if (*(it + 1) == "<") {
+					(*temp).opcode = 1;
+				}
+				if (*(it + 1) == ">=") {
+					(*temp).opcode = 2;
+				}
+				if (*(it + 1) == "<=") {
+					(*temp).opcode = 3;
+				}
+				if (*(it + 1) == "=") {
+					(*temp).opcode = 4;
+				}
+				if (*(it + 1) == "<>") {
+					(*temp).opcode = 5;
+				}
+				sqlcmd.condList.push_back(*temp);
+				it += 4;
+			}
+			Condition * temp = new Condition;
+			(*temp).attriName = *it;
+			(*temp).cond = *(it + 2);
+			if (*(it + 1) == ">") {
+				(*temp).opcode = 0;
+			}
+			if (*(it + 1) == "<") {
+				(*temp).opcode = 1;
+			}
+			if (*(it + 1) == ">=") {
+				(*temp).opcode = 2;
+			}
+			if (*(it + 1) == "<=") {
+				(*temp).opcode = 3;
+			}
+			if (*(it + 1) == "=") {
+				(*temp).opcode = 4;
+			}
+			if (*(it + 1) == "<>") {
+				(*temp).opcode = 5;
+			}
+			sqlcmd.condList.push_back(*temp);
+		}
+		else 
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+	}
+
+	else if (*it == "quit" || *it == "exit")
+	{
+		return 0;
+	}
+
+	else if (*it == "execfile")
+	{
+		if (tokenizer.size() != 3)
+		{
+			cerr << "SQL Syntax Error! " << endl;
+			return 1;
+		}
+	}
+
+	else 
+	{
+		cerr << "SQL Syntax Error! " << endl;
+		return 1;
+	}
+
+	return 0;
+	
+}
+
+void Interpreter::execute(void)
+{
+
+}
+
+void Interpreter::executefile(void)
+{
+	file.open(tokenizer[1], ios::in);
+
+	if (!file)
+	{
+		cerr << "Cannot open file \"" << tokenizer[1] << "\"!" << endl;
+		return;
+	}
+
+	string line;
+
+
+	while (!file.eof())
+	{
+		getline(file, line);
+		tokenizer.clear();
+		query = trim(line);
+		execute();
+	}
+	file.close();
+}
+
+string & Interpreter::trim(string & str)
+{
+	string res = "";
+
+	vector<string>::iterator it;
+
+	vector<string> token;
+
+	if (str != "")
+	{
+		str.erase(0, str.find_first_not_of(" \n\r\t"));
+		str.erase(str.find_last_not_of(" \n\r\t") + 1);
+	}
+	else
+		return str;
+
+	str.append(" ");
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		char ch = str[i];
+
+		if (ch == '(')
+		{
+			if (res != "") 
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+				
+			res = "";
+			token.push_back("(");
+			tokenizer.push_back("(");
+		}
+
+		else if (ch == ')')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+			token.push_back(")");
+			tokenizer.push_back(")");
+		}
+
+		else if (ch == '*')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+			token.push_back("*");
+			tokenizer.push_back("*");
+		}
+
+		else if (ch == ';')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+			token.push_back(";");
+			tokenizer.push_back(";");
+		}
+
+		else if (ch == ',')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+			token.push_back(",");
+			tokenizer.push_back(",");
+		}
+
+		else if (ch == '=')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+			token.push_back("=");
+			tokenizer.push_back("=");
+		}
+
+		else if (ch == '>')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+
+			if (str[i + 1] == '=')
+			{
+				token.push_back(">=");
+				tokenizer.push_back(">=");
+				i++;
+			}
+			else 
+			{
+				token.push_back(">");
+				tokenizer.push_back(">");
+			}
+				
+		}
+
+		else if (ch == '<')
+		{
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+
+			if (str[i + 1] == '=')
+			{
+				token.push_back("<=");
+				tokenizer.push_back("<=");
+				i++;
+			}
+			else if (str[i + 1] == '>') 
+			{
+				token.push_back("<>");
+				tokenizer.push_back("<>");
+				i++;
+			}
+			else
+			{
+				token.push_back("<");
+				tokenizer.push_back("<");
+			}
+
+		}
+
+
+		else if (ch != ' ' && ch != '\n' && ch != '\t' && ch != '\r' && ch != '"' && ch != 0x27)
+		{
+			res += ch;
+		}
+
+		else {
+			if (res != "")
+			{
+				token.push_back(res);
+				tokenizer.push_back(res);
+			}
+			res = "";
+		}
+	}
+	res = "";
+	it = token.begin();
+	res += *it;
+
+	for (it = it + 1; it != token.end(); it++)
+		res += " " + *it;
+
+	str = res;
+	return str;
+}
+
+void Interpreter::token_debug(void)
+{	
+	int i = 0;
+	cout << "-----------------------------" << endl;
+	cout << "Token: " << endl;
+	for (vector<string>::iterator it = tokenizer.begin(); it != tokenizer.end(); it++)
+	{
+		cout << "Token " << i++ << ": " << *it << endl;
+	}
+	cout << "-----------------------------" << endl;
+}
+
+void Interpreter::parse_debug(void)
+{
+	sqlcmd.tableInfo.show();
+}
--- MiniSQL/MiniSQL/Interpreter.h
+++ MiniSQL/MiniSQL/Interpreter.h
@@ -1,33 +1,90 @@
-#ifndef INTERPRETER_H
-#define INTERPRETER_H
+#pragma once
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
-#include "MiniSQL.h"
+
 using namespace std;
 
-/* Clause修改为Minisql.h中的Condition类 */
-class Clause {
-	string attriName;
-	string cond;
-	int	opcode;
-	/*
-		#0: >
-		#1: <
-		#2: >=
-		#3. <=
-		#4: =
-		#5: <>
-	*/
+class Attribute {
+public:
+	string name;
+	int type;
+	bool PK; //primary key
+	bool unique;
+	Attribute() = default;
+	Attribute(string n, int t, bool p, bool u)
+		:name(n), type(t), PK(p), unique(u)
+	{ };
+	void show()
+	{
+		cout << name;
+		switch (type)
+		{
+		case 0:
+			cout << " int";
+			break;
+		case 1:
+			cout << " float";
+			break;
+		case 2:
+			cout << " char(" << type << ")";
+			break;
+		}
+		if (PK) {
+			cout << " Primary Key";
+		}
+		else if (unique) {
+			cout << " Unique";
+		}
+		cout << endl;
+	}
 };
 
-class SQLCommand {
+/* Table class in CatalogManager, only table info without records */
+class Table {
 public:
-	//ADD:
-	Table tableInfo;
-	Index indexInfo;
+	string name; //name of the table
+	int attriNum; //number of the attributes
+	vector<Attribute> attributes; //list of the attributes
+	int recNum;
+	int recLength; //length of a record(一个record有多少byte，固定值)
+	int blockNum;
+	int fileTail; //文件尾的offset作为Block读取的参考
+	Table() = default;
+	Table(string n) : name(n) { };
+	Table(string n, int attrNum, std::vector<Attribute> attrs)
+		: name(n), attriNum(attrNum), attributes(attrs), recNum(0), blockNum(1), fileTail(0)
+	{
+		int length = 0;
+		/*for (auto elem : attrs) {
+		length += elem.size();
+		}
+		recLength = length;*/
+	}
+	void show() {
+		cout << "table " << name << ":" << attriNum << " attributes" << endl;
+		for (auto elem : attributes) {
+			elem.show();
+		}
+	}
+};
+
+/* index class in IndexManager, information of a index created */
+class Index {
+public:
+	string tableName;
+	string indexName;
+	string attributeName;
+	int type; //TODO:创建的时候赋值type
+	Index() = default;
+	Index(string tbName, string idName, string attr)
+		:tableName(tbName), indexName(idName), attributeName(attr)
+	{ }  //TODO:在API内赋值type
+};
+
+//class SQLCommand {
 	/*
 		Example1: select * from t1 where a1 = c1 and where a2 >= c2;
 
@@ -67,6 +124,9 @@ public:
 		(5) valuesList = Empty Set
 	*/
 
+	// 操作的表 E.g. t1
+	//string tableName;
+
 	// 对该表的操作 E.g. select, insert, create table, drop, etc.
 	/*
 		对应关系：
@@ -80,37 +140,62 @@ public:
 
 		(quit和execfile直接判断跳转)
 	*/
-	int opName;
+	//int opName;
 
 	// 操作的列名（属性/Attribute）
 	// 仅适用于 select, create index
 	// 注意: attriNameList[0] == "*" 时表示select所有列
-	vector<string> attriNameList;
+	//vector<string> attriNameList;
 
 	// 条件子句 E.g  ... where attri opcode cond
 	//	Like		... where s_id	   =   "111"
 	// 仅适用于 select, delete
-	vector<Condition> condList;
+	//vector<Clause> condList;
 
 	// 赋值
 	// 仅适用于 insert. 
 	// 插入一条record时values值被解析到这个list中
+	//vector<string> valuesList;
+
+
+enum { Gt, Lt, Ge, Le, Eq, Ne };
+class Condition {
+public:
+	string attriName;
+	int columnNum; //属性在attributes中的index
+	string cond;
+	int opcode;
+};
+
+class SQLCommand {
+public:
+	Table tableInfo;
+	Index indexInfo;
+	int opName;
+	vector<Condition> condList;
 	vector<string> valuesList;
+
+	void clear(void) {
+		tableInfo.attributes.clear();
+		condList.clear();
+		valuesList.clear();
+	}
 };
 
 class Interpreter {
 public:
-	// 当status为false时停止执行
-    bool status;
 
 	// SQL语句
 	string query;
+
+	// tokenier
+	vector<string> tokenizer;
 
 	// 文件流
 	fstream file;
 
 	// 经过解析后的操作
-	SQLCommand op;
+	SQLCommand sqlcmd;
 
 	// Constructor
     Interpreter();
@@ -118,7 +203,7 @@ public:
 	// Destroyer
     ~Interpreter();
     
-	// 从键盘读取sql语句
+	// 从键盘读取sql语句, 并分词
 	void readquery(void);
 
 	/* 核心：解析器
@@ -126,9 +211,9 @@ public:
 	   1. 分析SQL语句，检查语法，生产对应语法树。
 	   2. 语句不符合语法则报错并返回非零值，若符合则执行。
 	*/
-	int  parser(void);
+	int parser(void);
 
-	// API接口，根据语法树执行相应的操作
+	// API接口，根据语法树执行相应的操作, 并检查语义正确性（逻辑层)。
 	void execute(void);
 
 	// 执行sql文件
@@ -137,6 +222,10 @@ public:
 	// 输出结果
 	void print(void);
 
-};
+	string & trim(string & str);
 
-#endif
+	void token_debug(void);
+
+	void parse_debug(void);
+
+};
